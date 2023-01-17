744db4919922fe2aba50aa83871988a936568a98
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-01-17 14:25:19 -0700

    tests favorites; tests request for learning resources

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::FavoritesController < ApplicationController
+  def index
+    if User.where(apikey: params[:api_key]).exists?
+      favorites = User.where(apikey: params[:api_key]).first.favorites
+      render json: FavoriteSerializer.format_favorites(favorites)
+    else
+      render json: 'better luck next time', status: 404
+    end
+  end
+
   def create
     if User.where(apikey: request.params[:api_key]).exists?
       user = User.where(apikey: request.params[:api_key]).first
       user.favorites.create!(fav_params)
-      render json: 'good jorb Homestar', status: 201
+      render json: 'Favorite added successfully', status: 201
     else
       render json: 'User not found', status: 404
     end
